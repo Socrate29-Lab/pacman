31cf171df3c7b7443d90269e57a6968bdc9277b8
Guillaume Sorgues <guillaume.sorgues@viacesi.fr>
2025-11-20 14:39:47 +0100

    on a fait du ménage et on ajoute la fonction déplacement

--- pacman.c
+++ pacman.c
@@ -7,8 +7,16 @@
 
 
 int main(){
+    // Initialisation de SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Erreur SDL_Init : %s\n", SDL_GetError());
+        return 1;
+    }
+    //Initialisation de l'ecran
     window();
+    //affichage du perso a l'ecran
     perso();
+    //Met fin a l'ecran au bout de 3 secondes (pour faire les tests)
     SDL_Delay(3000);
     terminate();
 }
