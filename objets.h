ec7da359db3a71290250eb0c271c78bad4f3145c
Guillaume Sorgues <guillaume.sorgues@viacesi.fr>
2025-12-08 14:44:10 +0100

    ajout des fichiers image pour les bonbons à manger

---
+++ objets.h
@@ -0,0 +1,10 @@
+#ifndef OBJETS_H
+#define OBJETS_H
+
+struct persos{
+    int posx;
+    int posy;
+    int statut; //Par exemple, en 1 Pacman se fait manger, en 2 Pacman mange les fantômes
+};
+
+#endif
