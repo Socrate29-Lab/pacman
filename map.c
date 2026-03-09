8d21a5ada81ea879007dd2afe392a6d349876920
Guillaume Sorgues <guillaume.sorgues@viacesi.fr>
2026-03-09 11:41:25 +0100

    new errors corrected

--- map.c
+++ map.c
@@ -6,7 +6,6 @@
 
 //Fonction pour afficher tous les assets sur la map une fois qu'ils ont été généré par generate_tile
 int generate_map(){
-    grille_init[31][28];
         for(int i=0; i<31; i++){
         for(int j=0; j<28; j++){
             int index = grille_init[i][j];
@@ -17,7 +16,6 @@ int generate_map(){
 
 //Fonction pour afficher les assets de la map une fois qu'on a modifié la grille de départ.
 int update_map(){
-    grille_deplacement[31][28];
         for(int i=0; i<31; i++){
             for(int j=0; j<28; j++){
                 int index = grille_deplacement[i][j];
